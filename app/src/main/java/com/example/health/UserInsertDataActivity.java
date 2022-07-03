7dce9e6436424c1cce9bfb54b481e08cf094097a
ruhan-mri <ruhan.9mri@gmail.com>
2022-07-03 15:07:40 +0600

    UI design healthApp

---
+++ app/src/main/java/com/example/health/UserInsertDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.health;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInsertDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_insert_data);
+    }
+}
